a8b6bde18d4bc6c20d7e1ad052208a1c0a806c30
Pedro Morais <pmorais@ualg.pt>
2026-09-22 14:59:30 -0500

    Update 01_fw_exit_dist.R

--- scripts/01_fw_exit_dist.R
+++ scripts/01_fw_exit_dist.R
@@ -14,7 +14,7 @@
 #' **DOI:** https://doi.org/10.1002/ecs2.70779
 #' 
 #' **Authors:** 
-#' Pedro Morais1,*, Anna Sturrock2,3+, Corey C. Phillis4, George Whitman2, 
+#' Pedro Morais1,*, Anna M. Sturrock2,3+, Corey C. Phillis4, George Whitman2, 
 #' Stephanie M. Carlson1, Rachel C. Johnson2,5
 #' 
 #' **Affiliations:**
